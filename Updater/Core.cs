e8186e20946f28a4aff5065a8b3139864fe05b68
EgyFalseX <mohaned.aly.omer@gmail.com>
2014-11-28 10:50:40 +0200

    Start Commit

---
+++ Updater/Core.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Updater
+{
+    public static class Core
+    {
+        public static void SetAllCommandTimeouts(object adapter, int timeout)
+        {
+            var commands = adapter.GetType().InvokeMember("CommandCollection", System.Reflection.BindingFlags.GetProperty | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic,
+                    null, adapter, new object[0]);
+            var sqlCommand = (IDbCommand[])commands;
+            foreach (var cmd in sqlCommand)
+            {
+                cmd.CommandTimeout = timeout;
+            }
+        }
+        public static string ZZReadMappedMemoryData()
+        {
+            string Data = string.Empty;
+            try
+            {
+                using (System.IO.MemoryMappedFiles.MemoryMappedFile mmf = System.IO.MemoryMappedFiles.MemoryMappedFile.OpenExisting("NICSQLToolsUpdateMap"))
+                {
+                    System.Threading.Mutex mutex = System.Threading.Mutex.OpenExisting("NICSQLToolsUpdateMapMutex");
+                    using (System.IO.MemoryMappedFiles.MemoryMappedViewStream stream = mmf.CreateViewStream())
+                    {
+                        System.IO.BinaryReader reader = new System.IO.BinaryReader(stream);
+                        Data = reader.ReadString();
+                    }
+                    mutex.WaitOne();
+                }
+                if (Data == string.Empty)
+                    System.Diagnostics.Process.GetCurrentProcess().Kill();
+                Properties.Settings.Default["DatabaseConnectionString"] = Data.Split(Convert.ToChar("||"))[0];
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("Memory-mapped file does not exist. Run Process A first.");
+            }
+            return Data.Split(Convert.ToChar("||"))[1];
+        }
+        public static byte[] GetFileData(string FilePath)
+        {
+            byte[] output = null;
+
+            string FileName = System.IO.Path.GetFileName(FilePath);
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.DatabaseConnectionString);
+            string commandString = String.Format("SELECT FileData FROM dbo.AppDependenceFile WHERE FileName = '{0}'", FileName);
+            SqlCommand cmd = new SqlCommand(commandString, con);
+            try
+            {
+                con.Open();
+                object obj = cmd.ExecuteScalar();
+                if (obj != null)
+                    output = (byte[])obj;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            con.Close();
+
+            return output;
+        }
+        public enum UpdaterArgsEnum
+        {
+            Upload = 1,
+            Download = 2
+        }
+    }
+}
---
+++ Updater/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Updater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Queue<string> FilesList { get; set; }
+        public MainWindow()
+        {
+            FilesList = new Queue<string>();
+
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ReadArgs();
+            //Update UI
+            pbEst.Maximum = FilesList.Count;
+            pbEst.Value = 0;
+            //Get Data From Database
+            GetUpdateData();
+        }
+        private void ReadArgs()
+        {
+            try
+            {
+                if (Environment.GetCommandLineArgs().Length <= 1)
+                {
+                    MessageBox.Show("No Data Found ...", "Shutdown", MessageBoxButton.OK, MessageBoxImage.Error);
+                    App.Current.Dispatcher.InvokeShutdown();
+                }
+                string arg = Environment.GetCommandLineArgs()[1];
+
+                string[] Data = FXFW.EncDec.Decrypt(arg, "FalseX").Split(Convert.ToChar("|"));
+                Properties.Settings.Default["DatabaseConnectionString"] = Data[0];
+                string ApplicationDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                for (int i = 1; i < Data.Length; i++)
+                    FilesList.Enqueue(String.Format(@"{0}\{1}", ApplicationDir, Data[i]));   
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButton.OK, MessageBoxImage.Information);
+                App.Current.Dispatcher.InvokeShutdown();
+            }
+            
+        }
+        private void GetUpdateData()
+        {
+            if (FilesList.Count == 0)// All Files Downloaded, Application Will Shutdown
+            {
+                MessageBox.Show("All Files Downloaded ... Application Will Shutdown", "Operation Ended", MessageBoxButton.OK, MessageBoxImage.Information);
+                App.Current.Dispatcher.InvokeShutdown();
+                return;
+            }
+
+            System.Threading.ThreadPool.QueueUserWorkItem((o) =>
+            {
+                string FileName = FilesList.Dequeue();
+                Dispatcher.Invoke(new Action(() => { lblFileName.Content = System.IO.Path.GetFileName(FileName); }));
+                
+                byte[] buffer = (byte[])Core.GetFileData(FileName);
+
+                if (System.IO.File.Exists(FileName))
+                    System.IO.File.Delete(FileName);
+                System.IO.FileStream fs = new System.IO.FileStream(FileName, System.IO.FileMode.CreateNew);
+                fs.Write(buffer,0, buffer.Length);
+                fs.Close();
+
+                Dispatcher.Invoke(new Action(() => { pbEst.Value = Convert.ToInt32(pbEst.Value) + 1; }));
+                GetUpdateData();
+                
+            });
+        }
+        private void Test()
+        {
+            string ApplicationDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            List<string> filePaths = new List<string>();
+            filePaths.Add(ApplicationDir + @"\DevExpress.BonusSkins.v14.1.dll");
+            filePaths.Add(ApplicationDir + @"\DevExpress.Dashboard.v14.1.Core.dll");
+            filePaths.Add(ApplicationDir + @"\DevExpress.RichEdit.v14.1.Core.dll");
+            filePaths.Add(ApplicationDir + @"\DevExpress.Utils.v14.1.dll");
+
+            Datasources.dsDataTableAdapters.QueriesTableAdapter adp = new Datasources.dsDataTableAdapters.QueriesTableAdapter();
+            Core.SetAllCommandTimeouts(adp, 0);
+
+            foreach (string item in filePaths)
+            {
+                string FileName = System.IO.Path.GetFileName(item);
+
+                byte[] buffer = System.IO.File.ReadAllBytes(item);
+                
+                adp.AddFileData(FileName, 1000, buffer);
+            }
+            
+            
+            
+            
+        }
+
+    }
+}
